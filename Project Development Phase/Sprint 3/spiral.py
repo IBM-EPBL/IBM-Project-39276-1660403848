faf22043dfcf281a2e1b4033661c14ee56c76738
Vinayak <vasuryavinu@gmail.com>
2022-11-17 00:34:12 +0530

    Update

--- Project Development Phase/Sprint 3/spiral.py
+++ Project Development Phase/Sprint 3/spiral.py
@@ -1,3 +1,4 @@
+# Importing necessary libraries
 import os
 import cv2
 import pickle
@@ -7,29 +8,33 @@ from sklearn.preprocessing import LabelEncoder
 from skimage import feature
 from sklearn.metrics import confusion_matrix, recall_score, precision_score, accuracy_score, f1_score
 
+# Quantifying Images
 def quantify(img):
   features = feature.hog(img, orientations = 9, pixels_per_cell = (10,10), cells_per_block = (2 , 2), transform_sqrt = True, block_norm = "L1")
   return features
 
-le = LabelEncoder()
 
+le = LabelEncoder()     # Label Encoder
+
+# Specifying Image Path
 fp_spiral_test_healthy = './TestData/spiral/healthy'
 fp_spiral_test_park = './TestData/spiral/parkinson'
 
 spiral_test_healthy = os.listdir(fp_spiral_test_healthy)
 spiral_test_park = os.listdir(fp_spiral_test_park)
 
-testXS = []
-testYS = []
+# Spliting into dependant and independant data
+testX = []
+testY = []
 
 for i in spiral_test_healthy:
   img = cv2.imread(fp_spiral_test_healthy + '/' + i)
   img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
   img = cv2.resize(img, (200,200))
-  img = cv2.threshold(img, 0,255,cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+  img = cv2.threshold(img, 0,255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
   features = quantify(img)
-  testXS.append(features)
-  testYS.append('healthy')
+  testX.append(features)
+  testY.append('healthy')
 
 for i in spiral_test_park:
   img = cv2.imread(fp_spiral_test_park + '/' + i)
@@ -37,38 +42,41 @@ for i in spiral_test_park:
   img = cv2.resize(img, (200,200))
   img = cv2.threshold(img , 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
   features = quantify(img)
-  testXS.append(features)
-  testYS.append('parkinson')
+  testX.append(features)
+  testY.append('parkinson')
 
-testYS = le.fit_transform(testYS)
+testY = le.fit_transform(testY) # Fit and Transforming / Encoding
 
+# Loading Pre-Trained Pickle Model
 with open('./models/spiral.pkl' , 'rb') as f:
     model = pickle.load(f)
 
-predS = model.predict(testXS)
+pred = model.predict(testX) # Predicting using model
 
 print("\nSPIRAL DRAWINGS:-")
 print("Model - Random Forest Classifier\n")
 
 print("Healthy --> 0    Parkinson --> 1")
-print("Prediction: ", predS)
-print("Actual: ", testYS)
+print("Prediction: ", pred)
+print("Actual: ", testY)
 
 print("\n\nMODEL EVALUATION:-\n")
 
-acc = accuracy_score(testYS, predS)
+# Performing model evaluation using various metrics by comparing prediction and actual values
+
+acc = accuracy_score(testY, pred)
 print("Accuracy: ", acc)
 
-rec = recall_score(testYS, predS)
+rec = recall_score(testY, pred)
 print("Recall: ", rec)
 
-pre = precision_score(testYS, predS)
+pre = precision_score(testY, pred)
 print("Precision: ", pre)
 
-f1 = f1_score(testYS, predS)
+f1 = f1_score(testY, pred)
 print("F1 score: ", f1)
 
-cnf = confusion_matrix(testYS, predS)
+cnf = confusion_matrix(testY, pred)
 print("Confusion Matrix: \n",cnf)
 
 plt.figure(figsize = (7,7))
--- Project Development Phase/Sprint 3/wave.py
+++ Project Development Phase/Sprint 3/wave.py
@@ -1,3 +1,4 @@
+# Importing necessary libraries
 import os
 import cv2
 import pickle
@@ -7,29 +8,32 @@ from sklearn.preprocessing import LabelEncoder
 from skimage import feature
 from sklearn.metrics import confusion_matrix, recall_score, precision_score, accuracy_score, f1_score
 
+# Quantifying Images
 def quantify(img):
   features = feature.hog(img, orientations = 9, pixels_per_cell = (10,10), cells_per_block = (2 , 2), transform_sqrt = True, block_norm = "L1")
   return features
 
-le = LabelEncoder()
+le = LabelEncoder()     # Label Encoder
 
+# Specifying Image Path
 fp_wave_test_healthy = './TestData/wave/healthy'
 fp_wave_test_park = './TestData/wave/parkinson'
 
 wave_test_healthy = os.listdir(fp_wave_test_healthy)
 wave_test_park = os.listdir(fp_wave_test_park)
 
-testXW = []
-testYW = []
+# Spliting into dependant and independant data
+testX = []
+testY = []
 
 for i in wave_test_healthy:
   img = cv2.imread(fp_wave_test_healthy + '/' + i)
   img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
   img = cv2.resize(img, (200,200))
-  img = cv2.threshold(img, 0,255,cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+  img = cv2.threshold(img, 0,255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
   features = quantify(img)
-  testXW.append(features)
-  testYW.append('healthy')
+  testX.append(features)
+  testY.append('healthy')
 
 for i in wave_test_park:
   img = cv2.imread(fp_wave_test_park + '/' + i)
@@ -37,38 +41,42 @@ for i in wave_test_park:
   img = cv2.resize(img, (200,200))
   img = cv2.threshold(img , 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
   features = quantify(img)
-  testXW.append(features)
-  testYW.append('parkinson')
+  testX.append(features)
+  testY.append('parkinson')
 
-testYW = le.fit_transform(testYW)
+testY = le.fit_transform(testY)   # Fit and Transforming / Encoding
 
+# Loading Pre-Trained Pickle Model
 with open('./models/wave.pkl' , 'rb') as f:
     model = pickle.load(f)
 
-predW = model.predict(testXW)
+pred = model.predict(testX)   # Predicting using model
 
 print("\nWAVE DRAWINGS:-")
 print("Model - K-Neighbor Classifier\n")
 
 print("Healthy --> 0    Parkinson --> 1")
-print("Prediction: ", predW)
-print("Actual: ", testYW)
+print("Prediction: ", pred)
+print("Actual: ", testY)
+
 
 print("\n\nMODEL EVALUATION:-\n")
 
-acc = accuracy_score(testYW, predW)
+# Performing model evaluation using various metrics by comparing prediction and actual values
+
+acc = accuracy_score(testY, pred)
 print("Accuracy: ", acc)
 
-rec = recall_score(testYW, predW)
+rec = recall_score(testY, pred)
 print("Recall: ", rec)
 
-pre = precision_score(testYW, predW)
+pre = precision_score(testY, pred)
 print("Precision: ", pre)
 
-f1 = f1_score(testYW, predW)
+f1 = f1_score(testY, pred)
 print("F1 score: ", f1)
 
-cnf = confusion_matrix(testYW, predW)
+cnf = confusion_matrix(testY, pred)
 print("Confusion Matrix: \n",cnf)
 
 plt.figure(figsize = (7,7))
